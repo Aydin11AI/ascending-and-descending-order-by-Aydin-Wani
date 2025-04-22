12b632bb41908396c3b91a06336ad496f747c5ac
Aydin Wani <aydinwania8@gmail.com>
2025-04-22 22:41:44 +0530

    Add files via upload

---
+++ ascending and descending order by Aydin Wani.py
@@ -0,0 +1,12 @@
+a=[1,3,4,6,2,7,9,8,0,5]
+print("Ascending")
+a.sort(reverse=False)
+for i in a:
+    print(i)
+print("decending")
+a.sort(reverse=True)
+for i in a:
+    print(i)
+#   a is a varable containing values in integer
+#   print is a function
+#   sort is a built-in method used to sort elements in a list in ascending order and dscending order
